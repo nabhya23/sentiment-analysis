513571d312fbb90c0a62888a3209b757c5d3dea3
nabhya23 <125913969+nabhya23@users.noreply.github.com>
2024-11-14 15:53:05 +0530

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -7,19 +7,17 @@ def preprocess_text(filename):
     # Read the text file
     text = open(filename, encoding='utf-8').read()
 
-    # Convert text to lowercase and remove punctuation
     lower_case = text.lower()
     cleaned_text = lower_case.translate(str.maketrans('', '', string.punctuation))
 
-    # Tokenize text and filter out stop words
     tokenized_words = word_tokenize(cleaned_text, 'english')
     final_words = [word for word in tokenized_words if word not in stopwords.words('english')]
 
-    # Save the final words to a file for the next script to use
+    
     with open('final_words.txt', 'w') as f:
         for word in final_words:
             f.write(f"{word}\n")
 
 
-# Run preprocessing when this file is executed
+
 preprocess_text('read.txt')
